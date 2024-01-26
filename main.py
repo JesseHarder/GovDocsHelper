78a08a68a36d726a289b59f8e0128299c0224df1
Jesse Harder <jhharder@scu.edu>
2024-01-25 19:59:40 -0800

    Wrote code for finding matches.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """A script to search for gov-docs of interest."""
 from csv import reader as csv_reader
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
 
 def simplify_sudoc_number(sudoc_number: str) -> str:
@@ -27,7 +27,8 @@ def perform_sudoc_match(
         scu_weeding_set_file: a path to the CSV file containing the weeding set.
 
     Returns:
-        None.
+        A dictionary mapping row numbers to the rows (list of strings with the first
+            value being the sudoc number)
     """
     # ------ Step 1 - Build our set of interest -----
     # Create the set of sudoc numbers from our weeding set that we want to locate in
@@ -44,10 +45,24 @@ def perform_sudoc_match(
             scu_sudoc_numbers.add(scu_sudoc_number)
 
     # ------ Step 2 - Search the FDLP reference -----
+    rows_of_interest: Dict[int, List[str]] = {}
+    with fdlp_reference_set_file.open("r") as file_pointer:
+        reader = csv_reader(file_pointer)
+        # Strip the first two rows. They don't have useful information.
+        next(reader)
+        next(reader)
+        # Iterate over the rest, looking for matches.
+        for row_number, row in enumerate(reader, 3):
+            # Get the sudoc number from the first column
+            fdlp_sudoc_number: str = row[0]
+            if simplify_sudoc_number(fdlp_sudoc_number) in scu_sudoc_numbers:
+                rows_of_interest[row_number] = row
+
+    return rows_of_interest
 
 
 if __name__ == "__main__":
-    perform_sudoc_match(
+    rows_of_interest = perform_sudoc_match(
         fdlp_reference_set_file=Path(
             "./spreadsheets/PreviousFDLPDisposalListOffers-2023-12-2.csv"
         ),
@@ -55,3 +70,4 @@ if __name__ == "__main__":
             "./spreadsheets/SantaClara20240124.csv"
         )
     )
+    print(len(rows_of_interest))
